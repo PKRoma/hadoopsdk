40bab4f3e0a828e58c422e86ffefb02fd92d875a
matt winkler <mwinkle@microsoft.com>
2013-01-23 16:00:49 -0800

    Adding build.bat and moving to version 0.2.0.1

---
+++ build.bat
@@ -0,0 +1,3 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Microsoft.Hadoop.WebHDFS\build.proj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Microsoft.Hadoop.MapReduce\build.proj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild .\Microsoft.Hadoop.Hive\LinqToHiveTranslator.sln
