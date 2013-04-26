b828352bc6b652567308a90cf99ac2ebf93be3a6
Maxim Lukiyanov <maxluk@microsoft.com>
2013-04-25 23:18:33 -0700

    Fix MRLib NuGet install script for WebClient

--- src/Microsoft.Hadoop.WebClient/src/Microsoft.Hadoop.WebClient/install.ps1
+++ src/Microsoft.Hadoop.WebClient/src/Microsoft.Hadoop.WebClient/install.ps1
@@ -3,9 +3,12 @@ $source = "$installPath\..\Microsoft.CompilerServices.AsyncTargetingPack.1.0.1\l
 $target = "$installPath\lib\net40\Microsoft.CompilerServices.AsyncTargetingPack.Net4.dll"
 copy $source $target
 
-Write-Host Setting MRLib items CopyToOutputDirectory=true
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.CombineDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.ReduceDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapReduce.dll").Properties.Item("CopyToOutputDirectory").Value = 1
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("MRRunner.exe").Properties.Item("CopyToOutputDirectory").Value = 1
+try {
+  Write-Host Setting MRLib items CopyToOutputDirectory=true
+  $project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.CombineDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
+  $project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
+  $project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.ReduceDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1
+  $project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapReduce.dll").Properties.Item("CopyToOutputDirectory").Value = 1
+  $project.ProjectItems.Item("MRLib").ProjectItems.Item("MRRunner.exe").Properties.Item("CopyToOutputDirectory").Value = 1
+} catch [Exception] {
+}
